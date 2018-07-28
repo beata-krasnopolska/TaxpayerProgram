4bde2868eb31c8b35ffaf09a11d0658c2f42a47e
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-07-28 20:49:08 +0200

    Add project files.

---
+++ TaxpayerProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaxpayerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxpayer[] payers = new Taxpayer[10];
+            for (int i = 0; i < payers.Length; i++)
+            {
+                payers[i] = new Taxpayer();
+                Console.WriteLine("Enter payer #{0} Social Security Number", i + 1);
+                payers[i].SocSecurNumb = Console.ReadLine();
+                Console.WriteLine("Enter payer #{0} yearly income", i+1);
+                payers[i].Income = double.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < payers.Length; i++)
+            {
+                Console.WriteLine("Tax Payer {0}, SSN {1}, income {2}, tax owed " +
+                    "{3} ", i + 1, payers[i].SocSecurNumb, payers[i].Income, payers[i].TaxOwed);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ TaxpayerProgram/Taxpayer.cs
@@ -0,0 +1,51 @@
+﻿namespace TaxpayerProgram
+{
+    class Taxpayer
+    {
+        private string socSecurNumb;
+        private double income;
+        private double taxOwed;
+        
+        public string SocSecurNumb
+        {
+            get
+            {
+                return socSecurNumb;
+            }
+            set
+            {
+                socSecurNumb = value;
+            }
+        }
+        public double Income
+        {
+            get
+            {
+                return income;
+            }
+            set
+            {
+                income = value;
+                TaxCalc();
+            }
+        }
+        public double TaxOwed
+        {
+            get
+            {
+                return taxOwed;
+            }
+        }
+        private void TaxCalc()
+        {
+            if (income < 30000)
+            {
+                taxOwed = income * .15;
+            }
+            else
+            {
+                taxOwed = income * .28;
+            }
+        }
+    }
+}
